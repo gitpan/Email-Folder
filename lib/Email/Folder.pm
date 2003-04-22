86125daf53120b3f2e841f2da763ea6e05927864
Richard Clamp <richardc@unixbeard.net>
2003-04-22 10:49:26 +0000

    Import of RCLAMP/Email-Folder-0.2 from CPAN.
    gitpan-cpan-distribution: Email-Folder
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         RCLAMP/Email-Folder-0.2.tar.gz
    gitpan-cpan-author:       RCLAMP
    gitpan-cpan-maturity:     released

--- lib/Email/Folder.pm
+++ lib/Email/Folder.pm
@@ -5,23 +5,21 @@ use Carp;
 use IO::File;
 use Email::Simple;
 use Email::FolderType qw/folder_type/;
-use UNIVERSAL::require;
 
 use vars qw($VERSION);
-
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 =head1 NAME
 
-Email::Folder - 
+Email::Folder -
 
 =head1 SYNOPSIS
 
-	use Email::Folder;
+ use Email::Folder;
 
-	my $folder = Email::Folder->new("some_file");
-	
-	print join "\n", map { $_->header("Subject") }, $folder->messages();
+ my $folder = Email::Folder->new("some_file");
+
+ print join "\n", map { $_->header("Subject") }, $folder->messages();
 
 =head1 METHODS
 
@@ -33,54 +31,53 @@ Takes the name of a folder;
 
 
 sub new {
-	my $class  = shift;
-	my $folder = shift || carp "Must provide a folder name\n";
-	my %self   = @_;
+    my $class  = shift;
+    my $folder = shift || carp "Must provide a folder name\n";
+    my %self   = @_;
 
-	carp "'$folder' does not exist\n" unless (-e $folder); 
-	
-	$self{_folder} = $folder;
-	$self{_type}   = folder_type($folder);
-		
-	return bless \%self, $class;
+    carp "'$folder' does not exist\n" unless (-e $folder);
+
+    $self{_folder} = $folder;
+    $self{_type}   = folder_type($folder);
+
+    return bless \%self, $class;
 }
 
 =head2 bless_message <message>
 
-Takes a raw RFC822 message and blesses it into a class. 
+Takes a raw RFC822 message and blesses it into a class.
 
 By default this is an Email::Simple object but could be overwritten.
 
-=cut  
+=cut
 
 sub bless_message {
-	my $self    = shift;
-	my $message = shift || die "You must pass a message\n";
+    my $self    = shift;
+    my $message = shift || die "You must pass a message\n";
 
-	return Email::Simple->new($message);
-
+    return Email::Simple->new($message);
 }
 
 
-=head2 messages 
+=head2 messages
 
 Returns a list of all the messages in a folder
 
 =cut
 
 sub messages {
-	my $self = shift;
-	return @{$self->{_messages}} if defined $self->{_messages};
-	
-	my $type = $self->{_type};
-	my $class = "Email::Folder::$type";
-	$class->require;
-	$class->can('messages') || croak "$class does not have method 'messages'";	
+    my $self = shift;
+    return @{$self->{_messages}} if defined $self->{_messages};
 
-	my @messages = map { $self->bless_message($_) } $class->messages($self->{_folder});
-	$self->{_messages} = \@messages;
+    my $type = $self->{_type};
+    my $class = "Email::Folder::$type";
+    eval "require $class";
+    $class->can('messages') || croak "$class does not have method 'messages'";
 
-	return @{$self->{_messages}};
+    my @messages = map { $self->bless_message($_) } $class->messages($self->{_folder});
+    $self->{_messages} = \@messages;
+
+    return @{$self->{_messages}};
 }
 
 =head1 AUTHOR
